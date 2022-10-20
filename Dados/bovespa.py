28d2a4c2a0e332d3890c17d02aed279d258625ad
Thiago <thiago_murakami@hotmail.com>
2022-10-20 11:05:53 -0300

    add file b3

---
+++ Dados/bovespa.py
@@ -0,0 +1,6 @@
+#importando a biblioteca panda
+# import pandas as pd
+
+# #df = pd.read_csv("C:\\Users\\Thiago\\Documents\\Projetos\\dio-desafio-ml\\Dados\\listadosFundos.csv", encoding="utf-8", sep=';')
+# df = pd.read_csv("C:/Users/Thiago/Documents/Projetos/dio-desafio-ml/Dados/listadosFundos.csv", encoding="utf-8")
+# print(df)
