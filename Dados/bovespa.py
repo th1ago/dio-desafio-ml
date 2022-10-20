6b38c048e8d7a38716bb81ecda5c918f8b0c5c5f
Thiago <thiago_murakami@hotmail.com>
2022-10-20 16:42:35 -0300

    fix param and add news

--- Dados/bovespa.py
+++ Dados/bovespa.py
@@ -1,7 +1,23 @@
+'''
+Intro the pandas libary
+'''
+
 #importando a biblioteca panda
 import pandas as pd
 
+#ignora linhas com erros - error_bad_lines=True
 df = pd.read_csv("C:\\Users\\Thiago\\Documents\\Projetos\\dio-desafio-ml\\Dados\\listadosFundos.csv", encoding="latin-1", sep=';')
-df.head()
 
-print(df)
+#total de linhas e colunas
+linhas = df.shape
+
+#somente o nome da coluna
+colunas = df.columns
+
+#as ultimas linhas
+ulitmas_linhas = df.tail(5)
+
+#retorna estatísticas dos dados
+descricao = df.describe()
+
+print(colunas)
