7d55934f1cd047f3af19e63b11d5ea69fd158b07
Thiago <thiago_murakami@hotmail.com>
2022-10-27 13:10:39 -0300

    add file conjunto

---
+++ Listas/conjuntos.py
@@ -0,0 +1,12 @@
+'''
+Um set 'e uma colecao que nao possui objetos repetidos
+Utilizado para eliminar itens duplicado de um iteravel
+'''
+num = set([1, 1, 2, 3, 3, 4])
+print(num)
+
+#acessando os valores de um set
+numeros = {1,2,3,2}
+#transformar ela em uma lista para acessar
+listas = list(numeros)
+print(listas)
