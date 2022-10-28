b456feecf3d4b66e3edf780d872b9ced2dd2d7aa
Thiago <thiago_murakami@hotmail.com>
2022-10-28 11:50:03 -0300

    add news method

--- Listas/conjuntos.py
+++ Listas/conjuntos.py
@@ -2,6 +2,9 @@
 Um set 'e uma colecao que nao possui objetos repetidos
 Utilizado para eliminar itens duplicado de um iteravel
 '''
+import enum
+
+
 num = set([1, 1, 2, 3, 3, 4])
 print(num)
 
@@ -10,3 +13,9 @@ numeros = {1,2,3,2}
 #transformar ela em uma lista para acessar
 listas = list(numeros)
 print(listas)
+
+#quando for querer saber o indice
+#utiliza a funcao enumerate
+carros = {"celta", "hb20", "polo"}
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
