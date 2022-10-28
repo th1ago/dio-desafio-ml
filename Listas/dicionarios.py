78feab712bb5fab9980cbaa1b43256a3d56ac00e
Thiago <thiago_murakami@hotmail.com>
2022-10-28 12:01:58 -0300

    add file dict

---
+++ Listas/dicionarios.py
@@ -0,0 +1,11 @@
+from numpy import NaN
+
+
+pessoa = {"nome": "Thiago", "idade": NaN}
+print(pessoa)
+
+pessoas = dict(nome="thiago", idade=NaN)
+print(pessoas)
+
+pessoa["telefone"] = "321-321"
+print(pessoa)
