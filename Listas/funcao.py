a0d8f430671c0ff553b4ebb66ca7b27b5092b236
Thiago <thiago_murakami@hotmail.com>
2022-10-28 16:35:04 -0300

    add method

--- Listas/funcao.py
+++ Listas/funcao.py
@@ -1,10 +1,6 @@
 '''
 bloco de codigo
 '''
-
-import re
-
-
 def calcular_tot(numero):
     return sum(numero)
 
@@ -14,14 +10,33 @@ def retorna_antecessor_e_sucessor(numero):
 
     return antecessor, sucessor
 
-#print(calcular_tot([10, 20])) #30
-#print(retorna_antecessor_e_sucessor(10)) #(9,11)
+print(calcular_tot([10, 20])) #30
+print(retorna_antecessor_e_sucessor(10)) #(9,11)
 
-# passagem de argumentos
+'''
+Passagem de argumentos
+'''
 def salvar_carro(marca, modelo, ano, placa):
     print(marca, modelo, ano, placa)    
 
 salvar_carro("Fiat", "Doblo", 2000, "123ABC")
 salvar_carro(marca="Fiat", modelo="Doblo", ano=2000, placa="123ABC")
-# passando um dict
+# passando um dict **
 salvar_carro(**{"marca":"Fiat", "modelo":"Doblo", "ano":2000, "placa":"123ABC"})
+
+# Args(*tupla) e kwargs(**dicionarios)
+def exibir_poema(data_extenso, *args, **kwargs):
+    texto = "\n".join(args)
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    print(mensagem)
+
+
+exibir_poema(
+    "Sexta, 1 de janeiro 1222"
+    "Zen of Python",
+    "Namespaces are one honking great idea -- let's do more of those!",
+    autor="Tim Peters",
+    ano=1999,
+)
+
