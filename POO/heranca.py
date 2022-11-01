cc0c842653b58ed669fbfc0318d976f9cc5cf171
Thiago <thiago_murakami@hotmail.com>
2022-11-01 14:04:29 -0300

    add new method

--- POO/heranca.py
+++ POO/heranca.py
@@ -9,6 +9,9 @@ class Veiculo():
 
     def ligar_motor(self):
         print("ligando o motor")
+    
+    def __str__(self):
+        return self.cor
 
 class Motocicleta(Veiculo):
     pass
@@ -17,8 +20,17 @@ class Carro(Veiculo):
     pass
 
 class Caminhao(Veiculo):
-    pass
+    def __init__(self, cor, placa, rodas, carregado):
+        # herdando as caracteristica do pai com o SUPER
+        super().__init__(cor, placa, rodas)
+        self.carregado = carregado
+    
+    def esta_carregado(self):
+        print(f"{'Sim' if self.carregado else 'Nao'} estou carregado")
 
 moto = Motocicleta("red", "123-abc", 2)
-print(moto)
 moto.ligar_motor()
+
+truck = Caminhao("preto", "1234-qwe", 8, True)
+truck.ligar_motor()
+truck.esta_carregado()
