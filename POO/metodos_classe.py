45ff51f4805dbd241b9929ddf9a9cb4233b25260
Thiago <thiago_murakami@hotmail.com>
2022-11-03 14:35:10 -0300

    add new file classmethod

---
+++ POO/metodos_classe.py
@@ -0,0 +1,19 @@
+'''
+Metodos de classe estao ligado a classe e nao ao objeto.
+Pois recebem um param que aponta para a classe e nao para a instancia do objeto 
+'''
+
+class Pessoa:
+    def __init__(self, pessoa, idade):
+        self.pessoa = pessoa
+        self.idade = idade
+
+    # transformar em metodo de classe
+    # agora utiliza cls
+    @classmethod
+    def criar_data_born(cls, ano, mes, dia, nome):
+        idade = 2022 - ano
+        return cls(nome, idade)
+
+p = Pessoa.criar_data_born(1988, 1, 1, "Thiago")
+print(p)
